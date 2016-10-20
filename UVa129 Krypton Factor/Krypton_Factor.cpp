5694d09374f5209f1355934cac52acf990d6c39d
TairanHu <1796754043@qq.com>
2016-10-21 00:34:21 +0800

    UVa129

---
+++ UVa129 Krypton Factor/Krypton_Factor.cpp
@@ -0,0 +1,65 @@
+
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int n, L, cnt;
+int S[100];
+
+int dfs(int cur)
+{
+    if(cnt++ == n)
+    {
+        for(int i = 0; i < cur; i++)
+        {
+            if(i%64 == 0 && i > 0)
+                cout << endl;
+            else if(i%4 == 0 && i > 0)
+                cout << " ";
+            cout << (char)('A' + S[i]);
+        }
+        cout << endl << cur << endl;
+        return 0;
+    }
+
+    for(int i = 0; i < L; i++)
+    {
+        S[cur] = i;
+        int ok = 1;
+        for(int j = 1; j*2 <= cur+1; j++)
+        {
+            int equal = 1;
+            for(int k = 0; k < j; k++)
+            {
+                if(S[cur-k] != S[cur-k-j])
+                {
+                    equal = 0;
+                    break;
+                }
+            }
+            if(equal)
+            {
+                ok = 0;
+                break;
+            }
+        }
+        if(ok)
+        {
+            if(!dfs(cur+1))
+                return 0;
+        }
+    }
+    return 1;
+}
+
+int main()
+{
+    while(cin >> n >> L && n > 0)
+    {
+        cnt = 0;
+        dfs(0);
+    }
+    return 0;
+}
